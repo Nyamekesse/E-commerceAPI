49654af8a7bf8db4ea1b666f614230a0d9973b38
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-03 17:53:38 +0000

    loaded environment variables and configured database connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
+using E_commerceAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUserName = Environment.GetEnvironmentVariable("DB_USERNAME");
+
+if (string.IsNullOrEmpty(dbName) || string.IsNullOrEmpty(dbHost) || string.IsNullOrEmpty(dbPassword) || string.IsNullOrEmpty(dbUserName))
+{
+    Console.WriteLine("Provide all values for environment variables");
+    return;
+}
 
 // Add services to the container.
 
@@ -6,6 +22,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDBContext>(option => { option.UseNpgsql($"Host={dbHost}; Database={dbName}; Username={dbUserName}; Password={dbPassword}"); });
 
 var app = builder.Build();
 
