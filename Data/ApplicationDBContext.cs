df2d91c96058502a718b6b4e5c95a9f1e2428e0e
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-03 17:50:45 +0000

    defined and configured ApplicationDBContext

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_commerceAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
