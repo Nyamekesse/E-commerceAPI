4e85c8d20e6e682fc9273510418e3b168d24e635
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-03 18:09:30 +0000

    defined Contact and Product domain models

---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace E_commerceAPI.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LasttName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Message { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}
