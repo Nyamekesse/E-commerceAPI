bcf2674fea342fd1f1844a1c16fbdebfba4c5969
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-03 18:31:14 +0000

    modified attributes to specify contraints

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,12 +1,21 @@
-﻿namespace E_commerceAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+
+        [MaxLength(50)]
         public string FirstName { get; set; } = "";
+        [MaxLength(50)]
         public string LasttName { get; set; } = "";
+        [MaxLength(50)]
         public string Email { get; set; } = "";
+
+        [MaxLength(20)]
         public string Phone { get; set; } = "";
+        [MaxLength(50)]
         public string Subject { get; set; } = "";
         public string Message { get; set; } = "";
 
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,16 +1,22 @@
-﻿namespace E_commerceAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Name { get; set; } = "";
+        [MaxLength(50)]
         public string Brand { get; set; } = "";
+        [MaxLength(50)]
         public string Category { get; set; } = "";
-
+        [Precision(16, 2)]
         public decimal Price { get; set; }
 
         public string Description { get; set; } = "";
-
+        [MaxLength(100)]
         public string ImageFileName { get; set; } = "";
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
