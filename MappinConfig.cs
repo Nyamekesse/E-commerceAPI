6e79afc8cabd5b800d603ea2f7af6d3fcf4158eb
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-04 14:19:39 +0000

    defined auto mapping for contact model and dto

---
+++ MappinConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using E_commerceAPI.Models;
+using E_commerceAPI.Models.DTO;
+
+namespace E_commerceAPI
+{
+    public class MappinConfig : Profile
+    {
+        public MappinConfig()
+        {
+            CreateMap<Contact, ContactRequestDTO>().ReverseMap();
+            CreateMap<Contact, ContactUpdateDTO>().ReverseMap();
+        }
+    }
+}
