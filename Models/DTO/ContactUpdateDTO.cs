df08b9efe4c44e3c0a73b138a0500eb882408d25
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-04 14:18:47 +0000

    added contact request and contact update DTO

---
+++ Models/DTO/ContactUpdateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models.DTO
+{
+    public class ContactUpdateDTO
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; } = "";
+        [Required, MaxLength(50)]
+        public string LasttName { get; set; } = "";
+        [Required, EmailAddress, MaxLength(50)]
+        public string Email { get; set; } = "";
+
+        [MaxLength(20)]
+        public string? Phone { get; set; }
+        [Required, MaxLength(50)]
+        public string Subject { get; set; } = "";
+        [Required, MinLength(20), MaxLength(4000)]
+        public string Message { get; set; } = "";
+    }
+}
