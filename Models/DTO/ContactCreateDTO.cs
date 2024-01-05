d4abd93606dff3e8c5397100d0457979fd0f7975
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-05 01:44:04 +0000

    defined create contact DTO

---
+++ Models/DTO/ContactCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models.DTO
+{
+    public class ContactCreateDTO
+    {
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; } = "";
+        [Required, MaxLength(50)]
+        public string LasttName { get; set; } = "";
+        [Required, EmailAddress, MaxLength(50)]
+        public string Email { get; set; } = "";
+
+        [MaxLength(20)]
+        public string? Phone { get; set; }
+
+        public int SubjectId { get; set; }
+        [Required, MinLength(20), MaxLength(4000)]
+        public string Message { get; set; } = "";
+    }
+}
