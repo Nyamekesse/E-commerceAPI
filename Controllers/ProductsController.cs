6835e0bbebdc2ab475202eacc092eb9c42f68475
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-07 21:57:52 +0000

    created endpoint to get all products

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using E_commerceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(ApplicationDBContext context) : ControllerBase
+    {
+        private readonly ApplicationDBContext _context = context;
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.FromSqlRaw("SELECT * FROM Products").ToList();
+            return Ok(products);
+        }
+    }
+}
