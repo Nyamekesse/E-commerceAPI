1aa157b94be28934acb65ee51a17c55f6fbfc9a0
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-07 22:27:58 +0000

    added DTO for creating products

---
+++ Models/DTO/ProductCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models.DTO
+{
+    public class ProductCreateDTO
+    {
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = "";
+        [Required, MaxLength(50)]
+        public string Brand { get; set; } = "";
+        [Required, MaxLength(50)]
+        public string Category { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+        [Required]
+        public required IFormFile ImageFile { get; set; }
+    }
+}
