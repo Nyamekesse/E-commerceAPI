25732dab251a56b4b54fda6214ab0cb40593c398
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-07 22:28:26 +0000

    added DTO for updating products

---
+++ Models/DTO/ProductUpdateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models.DTO
+{
+    public class ProductUpdateDTO
+    {
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = "";
+        [Required, MaxLength(50)]
+        public string Brand { get; set; } = "";
+        [Required, MaxLength(50)]
+        public string Category { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+        [Required]
+        public IFormFile? ImageFile { get; set; }
+    }
+}
