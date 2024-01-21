c439538351f2c3488eb9bbcb276cfdc996c73458
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-21 21:52:31 +0000

    added dbset for users table

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -8,6 +8,7 @@ namespace E_commerceAPI.Data
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Subject> Subjects { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
