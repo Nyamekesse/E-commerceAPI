02b366135d8279e504abf9a9eda57b14ee1633c6
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-21 21:52:51 +0000

    created users model

---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models
+{
+    [Index("Email", IsUnique = true)]
+    public class User
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string FirstName { get; set; } = "";
+        [MaxLength(100)]
+        public string LastName { get; set; } = "";
+        [MaxLength(100)]
+        public string Email { get; set; } = "";
+        [MaxLength(20)]
+        public string Phone { get; set; } = "";
+        [MaxLength(100)]
+        public string Address { get; set; } = "";
+        [MaxLength(100)]
+        public string Password { get; set; } = "";
+        [MaxLength(20)]
+        public string Role { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
