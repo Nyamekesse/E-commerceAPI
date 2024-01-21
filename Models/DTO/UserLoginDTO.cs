7ed21f953ba1141f1b15c7ebb30bfaf18c1c6384
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-21 23:33:43 +0000

    created model dto for user login

---
+++ Models/DTO/UserLoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceAPI.Models.DTO
+{
+    public class UserLoginDTO
+    {
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
