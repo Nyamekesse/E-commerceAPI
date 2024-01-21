1a70d84609a02b60e9d345cbc1813ca1bb554c33
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-21 22:37:19 +0000

    created models for user creation and registration

---
+++ Models/DTO/UserProfileDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace E_commerceAPI.Models.DTO
+{
+    public class UserProfileDTO
+    {
+
+        public string FirstName { get; set; } = "";
+
+        public string LastName { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string Phone { get; set; } = "";
+
+        public string Address { get; set; } = "";
+        public string Role { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
