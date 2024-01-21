5d48fdda7ae451afb4140f6ca4f57e968ba0318c
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-21 22:36:41 +0000

    added controler to handle users operations

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using E_commerceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace E_commerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(IConfiguration configuration) : ControllerBase
+    {
+
+        string secretKey = Environment.GetEnvironmentVariable("SECRET")!;
+        private string CreateJWToken(User user)
+        {
+
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim("role", user.Role.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var token = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("JWTConfig:Issuer")!,
+                audience: configuration.GetValue<string>("JWTConfig:Audience")!,
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+
+
+    }
+}
