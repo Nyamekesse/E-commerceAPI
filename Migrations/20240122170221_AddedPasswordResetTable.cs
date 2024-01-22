79f27c3658c7286b3b6f3a1fe7ef61515d8596dc
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-22 18:52:02 +0000

    created password reset table via migration

---
+++ Migrations/20240122170221_AddedPasswordResetTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace E_commerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPasswordResetTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasswordResets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordResets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordResets_Email",
+                table: "PasswordResets",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordResets");
+        }
+    }
+}
